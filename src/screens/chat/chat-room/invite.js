cff68fb6d5faa9cc74e8333a81e2e8a7fe4f1667
Donghee Kim <kimdhoe1@gmail.com>
2019-02-20 16:45:47 +0900

    improve usability of invite modal

--- src/screens/chat/chat-room/invite.js
+++ src/screens/chat/chat-room/invite.js
@@ -27,6 +27,8 @@ const ANIM_DURATION = 150
 // -------------------------------------
 
 class Invite extends React.Component {
+  containerRef = React.createRef()
+
   // Animated values:
   opacity = new Animated.Value(0)
   translateY = this.opacity.interpolate({
@@ -44,66 +46,119 @@ class Invite extends React.Component {
 
   async componentDidUpdate(prevProps) {
     const { props } = this
+    const shouldShow = !prevProps.show && props.show
+    const shouldHide = prevProps.show && !props.show
 
-    // On show:
-    if (!prevProps.show && props.show) {
-      const users = await props.service.fetchAllUsers()
-
-      this.setState({
-        show: true,
-        isPending: false,
-        users: users.filter(id => id !== props.userID),
-      }, () => {
-        Animated.timing(this.opacity, {
-          toValue: 1,
-          duration: ANIM_DURATION,
-        }).start()
-      })
-      // On hide:
-    } else if (prevProps.show && !props.show) {
-      Animated.timing(this.opacity, {
-        toValue: 0,
-        duration: ANIM_DURATION,
-      }).start(() => {
-        this.setState({
-          isPending: true,
-          users: [],
-          show: false,
-        })
-      })
+    if (shouldShow) {
+      this.show()
+    } else if (shouldHide) {
+      this.hide()
     }
   }
 
+  componentWillUnmount() {
+    this.tearDown()
+  }
+
+  tearDown = () => {
+    window.removeEventListener('click', this.handleWindowClick)
+  }
+
+  show = async () => {
+    const { props } = this
+    const users = await props.service.fetchAllUsers()
+
+    this.setState({
+      show: true,
+      isPending: false,
+      users: users.filter(id => id !== props.userID),
+    }, () => {
+      Animated.timing(this.opacity, {
+        toValue: 1,
+        duration: ANIM_DURATION,
+      }).start(() => {
+        window.addEventListener('click', this.handleWindowClick)
+      })
+    })
+  }
+
+  hide = () => {
+    this.tearDown()
+    Animated.timing(this.opacity, {
+      toValue: 0,
+      duration: ANIM_DURATION,
+    }).start(() => {
+      this.setState({
+        isPending: true,
+        users: [],
+        show: false,
+      })
+    })
+  }
+
+  // handleWindowClick :: Event -> void
+  // Closes modal if the event target is this component or a descendant of it.
+  handleWindowClick = e => {
+    let node = e.target
+
+    while (node) {
+      if (node === this.containerRef.current.refs.node) return
+      node = node.parentNode
+    }
+
+    this.props.close()
+  }
+
   // handlePress :: string -> void
+  // Handles user selection.
   handlePress = receiverID => {
     const { props } = this
     props.service.invite(props.userID, receiverID, props.room, props.close)
   }
 
-  // renderUsers :: string[] -> React.Element
-  renderUsers = ({ users }) => (
-    <ul className={styles.users}>
-      {users.map(user => (
-        <li key={user} className={styles.user}>
-          <button
-            className={styles.userButton}
-            onClick={() => this.handlePress(user)}
-          >
-            {user}
-          </button>
-        </li>
-      ))}
-    </ul>
+  renderPlaceholder = () => (
+    <div className={styles.users}>
+      <div className={styles.placeholderItem} />
+      <div className={styles.placeholderItem} />
+      <div className={styles.placeholderItem} />
+    </div>
   )
+
+  renderUsers = () => {
+    const { users } = this.state
+
+    return (
+      <ul className={styles.users}>
+        {users.length > 0
+          ? users.map(user => (
+            <li key={user} className={styles.user}>
+              <button
+                className={styles.userButton}
+                onClick={() => this.handlePress(user)}
+              >
+                {user}
+              </button>
+            </li>
+          ))
+          : (
+            <p className={styles.emptyUsers}>
+              There's no one to invite at the moment.
+            </p>
+          )
+        }
+      </ul>
+    )
+  }
 
   render() {
     const { close } = this.props
-    const { show, isPending, users } = this.state
+    const { show, isPending } = this.state
 
     if (!show) return null
 
     return (
       <Animated.div
+        ref={this.containerRef}
         className={styles.container}
         style={{
           opacity: this.opacity,
@@ -120,8 +175,8 @@ class Invite extends React.Component {
 
         <div className={styles.content}>
           {isPending
-            ? null
-            : this.renderUsers({ users })
+            ? this.renderPlaceholder()
+            : this.renderUsers()
           }
         </div>
       </Animated.div>
